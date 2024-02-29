cd980dff3dd12664791a60b2e2868338cb11ea0d
Mārtiņš Možeiko <martins.mozeiko@gmail.com>
2024-02-28 23:16:52 -0800

    mesa 24.0.2

--- build.cmd
+++ build.cmd
@@ -1,9 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set LLVM_VERSION=18.1.0rc2
-set LLVM2_VERSION=18.1.0-rc2
-set MESA_VERSION=24.0.1
+set LLVM_VERSION=18.1.0rc4
+set LLVM2_VERSION=18.1.0-rc4
+set MESA_VERSION=24.0.2
 
 set PATH=%CD%\llvm\bin;%CD%\winflexbison;%PATH%
 
